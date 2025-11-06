ccbdef3177d161463e1efc49b74901bf48c7fba9
raiHancs <76402595+raiHancs@users.noreply.github.com>
2025-11-06 19:17:13 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+}
+export default App;
